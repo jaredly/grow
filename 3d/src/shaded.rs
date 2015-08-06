ef174d3c5f4d807f2dac92eabd79f4a2a1aceffc
Jared Forsyth <jared@jaredforsyth.com>
2015-08-06 13:55:55 -0700

    oh wow. opacity http://imgur.com/jdou0Ko.png

--- 3d/src/shaded.rs
+++ 3d/src/shaded.rs
@@ -115,7 +115,7 @@ uniform mat4 transform;
 uniform mat3 scale;
 varying vec3 uv_as_a_color;
 void main() {
-    uv_as_a_color  = vec3(uvs.xy, 0.0);
+    uv_as_a_color  = vec3(uvs.x, uvs.x, uvs.x);
     gl_Position = view * transform * mat4(scale) * vec4(position, 1.0);
 }
 ";
@@ -124,6 +124,6 @@ const ANOTHER_VERY_LONG_STRING: &'static str =
 "#version 120
 varying vec3 uv_as_a_color;
 void main() {
-    gl_FragColor = vec4(uv_as_a_color, 1.0);
+    gl_FragColor = vec4(uv_as_a_color, 0.5);
 }
 ";
