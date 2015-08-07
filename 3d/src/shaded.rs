af8f9990cfa5cb0b323b37c147116e0bdf3c3535
Jared Forsyth <jared@jaredforsyth.com>
2015-08-06 19:54:48 -0700

    now there's something that kinda looks like a tree http://imgur.com/8pAGSoq.png

--- 3d/src/shaded.rs
+++ 3d/src/shaded.rs
@@ -133,9 +133,20 @@ uniform mat4 view;
 uniform mat4 transform;
 uniform mat3 scale;
 varying vec3 uv_as_a_color;
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
 void main() {
     // float vtime = sin(time / 25.0) * 0.4 + 0.5;
-    uv_as_a_color  = vec3(uvs.y * 0.6, 1.0 - uvs.y * 0.6, uvs.y * .2);
+    float hue = uvs.y == 0 ? 1.0 / 3.0 : 0.075;
+    float sat = uvs.y == 0 ? 1.0 : 0.68;
+    float val = 0.5 * uvs.x; //  + (uvs.y == 0 ? 0.5 : 0.0); // uvs.y == 0 ? 
+    uv_as_a_color = hsv2rgb(vec3(hue, sat, val));
+    // uv_as_a_color  = vec3(uvs.y * 0.6, 1.0 - uvs.y * 0.6, uvs.y * .2);
     // uv_as_a_color  = vec3(uvs.x, 0.1, uvs.x / 2.0 + 0.5);
     gl_Position = view * transform * mat4(scale) * vec4(position, 1.0);
 }
